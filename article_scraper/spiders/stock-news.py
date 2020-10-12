3ca0859b4276c5a16b6d091802da37111213ed63
devwiz1028 <devwiz.sh@gmail.com>
2020-10-12 11:48:54 -0400

    add stock news scraper

---
+++ article_scraper/spiders/stock-news.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import w3lib.html
+import datetime
+
+class StockNewsSpider(scrapy.Spider):
+    name = "stock-news"
+    start_urls = [
+        'https://stocknews.com/top-stories',
+    ]
+
+    def parse(self, response):
+        for quote in response.css("#content .row .col-lg-8.col-md-7 div.margin-bottom h3 a"):
+            yield scrapy.Request('https://stocknews.com' + quote.attrib['href'], self.parse_article)
+
+    def parse_article(self, response):
+        yield {
+            'originalUrl' : response.url,
+            'url' : response.url, #to match lexis format
+            'title': response.css(".news-event h1.page-title::text").extract_first(),
+            'content': w3lib.html.remove_tags(response.css("#articlecontent > p").get()).strip(),
+            'authorName': response.css(".news-event .panel-body span.post-meta strong a::text").extract_first()[3:],
+            'publishedDate': response.css(".news-event .panel-body span.post-meta ~ a::text").extract_first(),
+            'harvestDate': datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ"),
+            'duplicateGroupId' : 'S' + str(hash(response.url)), #signify it came from scrapy and not lexis
+            'languageCode' : response.xpath("/html/@lang").get()
+        }
