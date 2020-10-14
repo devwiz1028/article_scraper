32a398a121ce266d02bd2bb17d0a98ba81555290
devwiz1028 <devwiz.sh@gmail.com>
2020-10-14 09:14:34 -0400

    add marketwatch and nasdaq scrapers

---
+++ article_scraper/spiders/marketwatch.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import w3lib.html
+import datetime
+
+class MarketWatchSpider(scrapy.Spider):
+    name = "marketwatch"
+    start_urls = [
+        'https://www.marketwatch.com/latest-news',
+    ]
+
+    def parse(self, response):
+        for quote in response.css(".collection__elements .element--article .article__figure a"):
+            url = quote.attrib['href']
+            if url.startswith('https://www.marketwatch.com'):
+                yield scrapy.Request(url, self.parse_article)
+
+    def parse_article(self, response):
+        yield {
+            'originalUrl' : response.url,
+            'url' : response.url, #to match lexis format
+            'title': response.css(".article__masthead h1.article__headline::text").extract_first().strip(),
+            'content': '\n'.join(map(lambda x: w3lib.html.remove_tags(x), response.css(".article__body p::text").extract())),
+            'authorName': response.css(".article__masthead .article__byline h4::text").extract_first().strip(),
+            'publishedDate': response.css(".article__masthead time.timestamp--pub::text").extract_first().split(':')[1].strip(),
+            'harvestDate': datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ"),
+            'duplicateGroupId' : 'S' + str(hash(response.url)), #signify it came from scrapy and not lexis
+            'languageCode' : response.xpath("/html/@lang").get()
+        }
---
+++ article_scraper/spiders/nasdaq.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import w3lib.html
+import datetime
+
+class NasdaqSpider(scrapy.Spider):
+    name = "nasdaq"
+    start_urls = [
+        'https://nasdaq.com/news-and-insights/topic/markets/world-markets',
+        'https://nasdaq.com/news-and-insights/topic/markets/stocks',
+    ]
+
+    def parse(self, response):
+        for quote in response.css(".content-feed__list a.content-feed__card-title-link"):
+            yield scrapy.Request('https://nasdaq.com' + quote.attrib['href'], self.parse_article)
+
+    def parse_article(self, response):
+        yield {
+            'originalUrl' : response.url,
+            'url' : response.url, #to match lexis format
+            'title': response.css("h1.article-header__headline span::text").extract_first().strip(),
+            'content': '\n'.join(map(lambda x: w3lib.html.remove_tags(x).strip(), response.css(".article-header + .body .body__content > :not(div)").extract())),
+            'authorName': response.css(".article-header__metadata .byline__author span.byline__name::text").extract_first().strip(),
+            'publishedDate': response.css(".article-header__metadata .timestamp time::text").extract_first().strip(),
+            'harvestDate': datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ"),
+            'duplicateGroupId' : 'S' + str(hash(response.url)), #signify it came from scrapy and not lexis
+            'languageCode' : response.xpath("/html/@lang").get()
+        }
